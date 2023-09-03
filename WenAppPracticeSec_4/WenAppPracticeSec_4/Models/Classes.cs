1509995e59203f026d72d8d5e0f0436488d3c0ac
kiruthigarajendran1903 <kiruthiga2807@gmail.com>
2023-09-03 23:01:38 +0530

    mainproject commit

---
+++ WenAppPracticeSec_4/WenAppPracticeSec_4/Models/Classes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WenAppPracticeSec_4.Models
+{
+    [Table("ClTable")]
+    public class Classes
+    {
+ 
+            [Key]
+            public int ClassId { get; set; }
+            [Required]
+            public string ClassName { get; set; }
+
+            public virtual Student Student { get; set; }
+
+        }
+    }
+
---
+++ WenAppPracticeSec_4/WenAppPracticeSec_4/Models/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WenAppPracticeSec_4.Models
+{
+ 
+        [Table("SubTable")]
+        public class Subject
+        {
+            [Key]
+            public int SubjectId { get; set; }
+            [Required]
+            public string SubjectName { get; set; }
+
+            public virtual Student Student { get; set; }
+
+        }
+    }
